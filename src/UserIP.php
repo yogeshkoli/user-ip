af5566cf640da3617987da845ccb871b88491de5
Yogesh Koli <info@yogeshkoli.com>
2017-07-17 12:30:53 +0530

    init class

---
+++ src/UserIP.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace YogeshKoli\UserIP;
+
+/**
+ * Class UserIP
+ * @package YogeshKoli\UserIP
+ */
+class UserIP
+{
+
+    /**
+     * @var null
+     */
+    private static $ip = NULL;
+
+
+    /**
+     * Get Current Active Client IP Address
+     *
+     * @return bool|null|string
+     */
+    static public function get()
+    {
+        if (!isset(self::$ip)) {
+            self::$ip = self::_check_ip();
+        }
+
+        return self::$ip;
+    }
+
+    /**
+     * @param $name
+     *
+     * @return string
+     */
+    private static function _get($name)
+    {
+        if (isset($_SERVER)) {
+            if (isset($_SERVER[$name])) {
+                return $_SERVER[$name];
+            }
+        } else {
+            if (getenv($name)) {
+                return getenv($name);
+            }
+        }
+
+        return 'UNKNOWN';
+    }
+
+    /**
+     * @return bool|string
+     */
+    private static function _check_ip()
+    {
+        if (self::_get('HTTP_X_FORWARDED_FOR')) {
+            return self::_get('HTTP_X_FORWARDED_FOR');
+        } else if (self::_get('HTTP_CLIENT_IP')) {
+            return self::_get('HTTP_CLIENT_IP');
+        } else if (self::_get('REMOTE_ADDR')) {
+            return self::_get('REMOTE_ADDR');
+        } else if (self::_get('HTTP_X_FORWARDED')) {
+            return self::_get('HTTP_X_FORWARDED');
+        } else if (self::_get('HTTP_FORWARDED_FOR')) {
+            return self::_get('HTTP_FORWARDED_FOR');
+        } else if (self::_get('HTTP_FORWARDED')) {
+            return self::_get('HTTP_FORWARDED');
+        } else {
+            return 'UNKNOWN';
+        }
+    }
+}
